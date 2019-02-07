aae92ab798feb1319055fa5981e06f6c27a5c910
hen <haeyon098@gmail.com>
2019-02-07 17:37:24 +0900

    디자인단계&데이터 단계
    디자인단계는 vuetify를 조작했다.
    데이터단계는 vuex 의 store아래 modules를 만들어서 로직을 연결했다.

--- apps/home-main/src/views/home-main.vue
+++ apps/home-main/src/views/home-main.vue
@@ -1,7 +1,21 @@
 <template>
-  <div id="home-main">
-    <h1>Home Main</h1>
-  </div>
+  <v-container id="home-main" fluid class="ma-0 pa-0">
+    <v-parallax dark height=400
+      src="https://img-s2.onedio.com/id-56ddb4ad606fe1702c52c2b9/rev-0/raw/s-b980e3a17a58f7b7e1021f2df2ddf4c918ca0401.jpg">
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 mb-3 yellow--text">Messages !</h1>
+        <h4 class="subheading">Send your message to the world !</h4>
+      </v-layout>
+    </v-parallax>
+  <v-container>
+    <v-layout align-center column>
+      <v-text-field id="1bEmail" v-model="email" label="E-mail"></v-text-field>
+
+      <v-btn id="btnLongin" color="warning" @click="clickLogin">Login</v-btn>
+
+    </v-layout>
+  </v-container>
+  </v-container>
 </template>
 <script>
 import { mapGetters, mapMutations, mapActions } from 'vuex'
@@ -11,6 +25,7 @@ export default {
 
   data () {
     return {
+      email: ''
     }
   },
 
@@ -29,9 +44,16 @@ export default {
 
   methods: {
     ...mapMutations({
+      loginEmail: 'users/email'
     }),
     ...mapActions({
-    })
+    }),
+    clickLogin () {
+      if (this.email) {
+        this.loginEmail(this.email)
+        this.$router.push({ name: 'messages-main' })
+      }
+    }
   },
 
   created () { console.log('CALL created()') },
--- apps/home-main/src/views/messages-main.vue
+++ apps/home-main/src/views/messages-main.vue
@@ -1,7 +1,48 @@
 <template>
-  <div id="maessages-main">
-    <h1>Messages Main</h1>
-  </div>
+  <v-container id="messages-main" fluid fill-height>
+    <v-layout align-center column>
+
+      <v-card width=600>
+          <v-toolbar color="cyan" dark>
+            <v-icon @click="clickHome">home</v-icon>
+            <v-toolbar-title>Messages</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list two-line>
+            <template v-for="(message, index) in messages">
+
+              <v-list-tile :key="(index+'tl')" class="pt-0 pb-0">
+                <v-list-tile-content>
+                  <v-list-tile-title >{{message.email}} <span class="caption cyan--text">- {{message.date}}</span></v-list-tile-title>
+                  <v-list-tile-sub-title >{{message.text}}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+
+              <v-divider :key="index"></v-divider>
+
+            </template>
+          </v-list>
+        </v-card>
+
+        <v-card width=600 class="mt-2">
+          <v-toolbar color="amber" dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title class="black--text"> Send messages
+            </v-toolbar-title>
+          </v-toolbar>
+
+          <v-text-field
+            label="Message text"
+            placeholder="please typing text"
+            outline
+            class="pa-3"
+            v-model="messageText"
+            @keypress="keypressMessageText"
+          ></v-text-field>
+
+        </v-card>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 import { mapGetters, mapMutations, mapActions } from 'vuex'
@@ -11,11 +52,14 @@ export default {
 
   data () {
     return {
+      messageText: ''
     }
   },
 
   computed: {
     ...mapGetters({
+      messages: 'messages/messages',
+      loginEmail: 'users/email'
     }),
 
     sample: {
@@ -31,7 +75,17 @@ export default {
     ...mapMutations({
     }),
     ...mapActions({
-    })
+      sendMessage: 'messages/sendMessage'
+    }),
+    clickHome () {
+      this.$router.push({ name: 'home-main' })
+    },
+    async keypressMessageText (e) {
+      if (e.key === 'Enter') {
+        console.log('messageText = ', this.messageText)
+        await this.sendMessage({ email: this.loginEmail, text: this.messageText })
+      }
+    }
   },
 
   created () { console.log('CALL created()') },
